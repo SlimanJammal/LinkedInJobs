8174817bcc00e40dc8cb093b62477ce6d2f679eb
SlimanJammal <sjamma03@campus.haifa.ac.il>
2024-04-02 00:57:48 +0300

    chatgpt api init test success.

--- main.py
+++ main.py
@@ -1,17 +1,35 @@
+import os
+
 import job_search, actions
 from selenium import webdriver
 
-driver = webdriver.Chrome()
-email = "naulub@makobj.store"
-password = "Fishpassword"
-actions.login(driver, email, password)
+# driver = webdriver.Chrome()
+# email = "rutjub@makobj.store"
+# password = "qwerty1234"
+# actions.login(driver, email, password)
+#
+# job_search = job_search.JobSearch(driver=driver, close_on_complete=False, scrape=False)
+# # job_search contains jobs from your logged in front page:
+# # - job_search.recommended_jobs
+# # - job_search.still_hiring
+# # - job_search.more_jobs
+#
+# job_listings = job_search.search("Machine Learning Engineer") # returns the list of `Job` from the first page
+#
+# print(job_listings)
 
-job_search = job_search.JobSearch(driver=driver, close_on_complete=False, scrape=False)
-# job_search contains jobs from your logged in front page:
-# - job_search.recommended_jobs
-# - job_search.still_hiring
-# - job_search.more_jobs
 
-job_listings = job_search.search("Machine Learning Engineer") # returns the list of `Job` from the first page
+from openai import OpenAI
 
-print(job_listings)
+client = OpenAI()
+
+completion = client.chat.completions.create(
+    model="gpt-3.5-turbo",
+    messages=[
+        {"role": "system",
+         "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."},
+        {"role": "user", "content": "Compose a poem that explains the concept of recursion in programming."}
+    ]
+)
+
+print(completion.choices[0].message)
