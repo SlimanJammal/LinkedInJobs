c408ef38902eee23d036c5b908ee38129355f94c
SlimanJammal <sjamma03@campus.haifa.ac.il>
2024-04-02 23:51:22 +0300

    testing_reserve_scraper.py updated job_title,location + minor bug fix

--- testing_reserve_scraper.py
+++ testing_reserve_scraper.py
@@ -38,80 +38,64 @@ def reload_page_if_needed(driver, original_url):
 
     return current_url == original_url , driver
 
-# driver = webdriver.Chrome()
-driver = webdriver.Chrome()
+def no_login_scraper(job_title,location):
+    # driver = webdriver.Chrome()
+    driver = webdriver.Chrome()
 
-original_url = "https://il.linkedin.com/jobs/eng-jobs-israel?trk=homepage-jobseeker_suggested-search&position=1&pageNum=0"
-driver.get(original_url)
-sleep(5)
-wait = WebDriverWait(driver, 10)
+    original_url = f"https://il.linkedin.com/jobs/{job_title}-jobs-{location}?trk=homepage-jobseeker_suggested-search&position=1&pageNum=0"
+    driver.get(original_url)
+    sleep(5)
+    wait = WebDriverWait(driver, 10)
 
-reload_threshold = 4
-while True:
-    status, driver = reload_page_if_needed(driver, original_url)
-    if status:
-        break
-    if reload_threshold == 0:
-        exit()
-    else:
-        reload_threshold -= 1
-
-driver.maximize_window()
-scroll_threshold = 1
-while True:
-    try:
-        scroll_to_bottom()
-        click_see_more_button()
-    except:
-        if scroll_threshold == 0:
+    reload_threshold = 4
+    while True:
+        status, driver = reload_page_if_needed(driver, original_url)
+        if status:
             break
-        delay = random.uniform(2, 3)
-        scroll_slightly_up()
-        time.sleep(delay+1)
-        scroll_threshold -= 1
-        # try:
-        #     if driver.find_element(By.CLASS_NAME,"inline-notification__text"):
-        #         print("You've viewed all jobs for")
-        #         break
+        if reload_threshold == 0:
+            exit()
+        else:
+            reload_threshold -= 1
 
-        # except:
-
-
-
-
-
-parent_ul = driver.find_element(By.CLASS_NAME, "jobs-search__results-list")
-
-job_cards = parent_ul.find_elements(By.CLASS_NAME, "base-card")
-
-jobs_data = []
-
-for i,job_card in enumerate(job_cards):
-    if i == 10:
-        break #todo remove temp for testing
-
-    job_title = job_card.find_element(By.CLASS_NAME, "base-search-card__title").text
-    company_name = job_card.find_element(By.CLASS_NAME, "base-search-card__subtitle").text
-    location = job_card.find_element(By.CLASS_NAME, "job-search-card__location").text
-
-    job_card.click()
-    try:
-        button = WebDriverWait(driver, 10).until(
-            EC.element_to_be_clickable((By.CSS_SELECTOR, "button[aria-label='i18n_show_more']"))
-        )
-        button.click()
-        time.sleep(1)
-    except:
-        if (i - 2) > 0:
-            job_cards[i-1].click()
-            sleep(2)
-        if(i-2 )> 0:
-            job_cards[i - 2].click()
+    driver.maximize_window()
+    scroll_threshold = 1
+    while True:
+        try:
+            scroll_to_bottom()
+            click_see_more_button()
+        except:
+            if scroll_threshold == 0:
+                break
             delay = random.uniform(2, 3)
             scroll_slightly_up()
-            time.sleep(delay + 1)
+            time.sleep(delay+1)
+            scroll_threshold -= 1
+            # try:
+            #     if driver.find_element(By.CLASS_NAME,"inline-notification__text"):
+            #         print("You've viewed all jobs for")
+            #         break
+
+            # except:
+
+
+
+
+
+    parent_ul = driver.find_element(By.CLASS_NAME, "jobs-search__results-list")
+
+    job_cards = parent_ul.find_elements(By.CLASS_NAME, "base-card")
+
+    jobs_data = []
+
+    for i,job_card in enumerate(job_cards):
+        if i == 10:
+            break #todo remove temp for testing
+
+        job_title = job_card.find_element(By.CLASS_NAME, "base-search-card__title").text
+        company_name = job_card.find_element(By.CLASS_NAME, "base-search-card__subtitle").text
+        location = job_card.find_element(By.CLASS_NAME, "job-search-card__location").text
+
         job_card.click()
-        sleep(2)
         try:
             button = WebDriverWait(driver, 10).until(
                 EC.element_to_be_clickable((By.CSS_SELECTOR, "button[aria-label='i18n_show_more']"))
@@ -119,33 +103,50 @@ for i,job_card in enumerate(job_cards):
             button.click()
             time.sleep(1)
         except:
-            print("job failed to read")
+            if (i - 2) > 0:
+                job_cards[i-1].click()
+                sleep(2)
+            if(i-2 )> 0:
+                job_cards[i - 2].click()
+                delay = random.uniform(2, 3)
+                scroll_slightly_up()
+                time.sleep(delay + 1)
+            job_card.click()
+            sleep(2)
+            try:
+                button = WebDriverWait(driver, 10).until(
+                    EC.element_to_be_clickable((By.CSS_SELECTOR, "button[aria-label='i18n_show_more']"))
+                )
+                button.click()
+                time.sleep(1)
+            except:
+                print("job failed to read")
+                continue
+
+        sleep(1)
+
+
+        try:# Get the job description text
+            job_description_element = WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located((By.CLASS_NAME, "show-more-less-html__markup"))
+            )
+            job_description = job_description_element.text
+
+            if job_description:
+                job_data = {
+                    "Job Title": job_title,
+                    "Company Name": company_name,
+                    "Location": location,
+                    "Job Description": job_description
+                }
+
+                jobs_data.append(job_data)
+                sleep(0.2)
+        except Exception as e:
+            print("An error occurred:", e)
             continue
 
-    sleep(1)
 
 
-    try:# Get the job description text
-        job_description_element = WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.CLASS_NAME, "show-more-less-html__markup"))
-        )
-        job_description = job_description_element.text
-
-        if job_description:
-            job_data = {
-                "Job Title": job_title,
-                "Company Name": company_name,
-                "Location": location,
-                "Job Description": job_description
-            }
-
-            jobs_data.append(job_data)
-            sleep(0.2)
-    except Exception as e:
-        print("An error occurred:", e)
-        continue
-
-
-
-while True:
-    pass
+    while True:
+        pass
