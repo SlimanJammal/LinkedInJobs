0c8b5a886ab73e909130fa0c8ff38de7e3564c23
SlimanJammal <sjamma03@campus.haifa.ac.il>
2024-05-13 04:45:28 +0300

    Airtable updated env vars

--- Airtable/main.py
+++ Airtable/main.py
@@ -1,30 +1,33 @@
 import requests
-AIRTABLE_BASE_ID = ""
-AIRTABLE_API_KEY = ""
-AIRTABLE_TABLE_NAME = ""
+import os
+from dotenv import load_dotenv
+load_dotenv(".env")
+AIRTABLE_BASE_ID = os.environ.get('AIRTABLE_BASE_ID')
+AIRTABLE_API_KEY = os.environ.get('AIRTABLE_API_KEY')
+AIRTABLE_TABLE_NAME = os.environ.get('AIRTABLE_TABLE_NAME')
+
 
 endpoint = f'https://api.airtable.com/v0/{AIRTABLE_BASE_ID}/{AIRTABLE_TABLE_NAME}'
 
-headers = {
-    "Authorization": f'Bearer {AIRTABLE_API_KEY}',
-    "Content-Type": "application/json"
-}
 
-data = {
-  "records": [
-    {
-      "fields": {
-          "name":"alex",
-          "email":"<EMAIL>"
-      }
-    },
-    {
-      "fields": {}
+
+def add_to_airtable(name="", email="", address=""):
+    headers = {
+        "Authorization": f'Bearer {AIRTABLE_API_KEY}',
+        "Content-Type": "application/json"
     }
-  ]
-}
 
+    data = {
+        "records": [
+            {
+                "fields": {
+                    "name": name,
+                    "email": email
+                }
+            }
+        ]
+    }
+    r = requests.post(endpoint, json=data, headers=headers)
+    print(r.status_code)
 
-r = requests.post(endpoint, json=data, headers=headers)
-print(r.status_code)
 
