2e4309b00d05aaa9fe9c3ad45d1abe8875a41b05
SlimanJammal <sjamma03@campus.haifa.ac.il>
2024-05-15 22:49:41 +0300

    added delete all records. added more fields and updated some fields to a more suitable way.

--- Airtable/main.py
+++ Airtable/main.py
@@ -18,6 +18,7 @@ def add_to_airtable(data_):
 
     data = data_
     r = requests.post(endpoint, json=data, headers=headers)
+    # print(r.content)
     print(r.status_code)
 
 
@@ -30,7 +31,8 @@ def add_jobs_list_to_airtable(jobs_list):
     data = []
     size = 0
     for job in jobs_list:
-        record = {"fields": {"job_title": job[0],
+        record = {
+                  "fields": {"job_title": job[0],
                              "company": job[1],
                              "experience": job[2],
                              "location": job[3],
@@ -48,4 +50,40 @@ def add_jobs_list_to_airtable(jobs_list):
             data = []
             size = 0
 
-# add_jobs_list_to_airtable(jobs_list_temp)
+    if len(data):
+        data_ = {"records": data}
+        add_to_airtable(data_)
+
+jobs_list_temp = [
+    ['Software Engineer', 'ABC Company', '3 years', 'New York', 'Full-time', 'https://example.com/job1'],
+    ['Data Scientist', 'XYZ Corporation', '2 years', 'San Francisco', 'Remote', 'https://example.com/job2'],
+    ['Product Manager', '123 Enterprises', '5 years', 'London', 'Part-time', 'https://example.com/job3'],
+    ['UX Designer', 'Tech Innovations Ltd.', '4 years', 'Berlin', 'Full-time', 'https://example.com/job4'],
+    ['Marketing Analyst', 'Global Marketing Solutions', '3 years', 'Paris', 'Contract', 'https://example.com/job5'],
+    ['Financial Analyst', 'Finance Unlimited', '2 years', 'Tokyo', 'Full-time', 'https://example.com/job6'],
+    ['Software Developer', 'Tech Solutions Inc.', '3 years', 'Sydney', 'Remote', 'https://example.com/job7'],
+    ['HR Manager', 'Human Resources Experts', '5 years', 'Toronto', 'Full-time', 'https://example.com/job8'],
+    ['Sales Representative', 'SalesPro', '1 year', 'Dubai', 'Commission-based', 'https://example.com/job9'],
+    ['Graphic Designer', 'Creative Designs Co.', '2 years', 'Los Angeles', 'Part-time', 'https://example.com/job10'],
+
+]
+add_jobs_list_to_airtable(jobs_list_temp)
+
+
+
+def delete_all_records():
+    base_url = f'https://api.airtable.com/v0/{AIRTABLE_BASE_ID}/{AIRTABLE_TABLE_NAME}'
+    headers = {
+        'Authorization': f'Bearer {AIRTABLE_API_KEY}',
+        'Content-Type': 'application/json'
+    }
+    response = requests.get(base_url, headers=headers)
+    data = response.json()
+    record_ids = [record['id'] for record in data['records']]
+    for record_id in record_ids:
+        delete_url = f'{base_url}/{record_id}'
+        requests.delete(delete_url, headers=headers)
+    print("All records have been deleted.")
+
+
+
