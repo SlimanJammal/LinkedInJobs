f6444e947bb65115db0d923f7d15da5d4d75a5dd
SlimanJammal <sjamma03@campus.haifa.ac.il>
2024-05-21 18:59:20 +0300

    added tables to the airtable, a table for each field CS EE AND ME, and updated all the functions accordingly. -- late push

--- Airtable/main.py
+++ Airtable/main.py
@@ -5,12 +5,23 @@ from dotenv import load_dotenv
 load_dotenv(".env")
 AIRTABLE_BASE_ID = os.environ.get('AIRTABLE_BASE_ID')
 AIRTABLE_API_KEY = os.environ.get('AIRTABLE_API_KEY')
-AIRTABLE_TABLE_NAME = os.environ.get('AIRTABLE_TABLE_NAME')
+AIRTABLE_TABLE_NAME_1 = os.environ.get('AIRTABLE_TABLE_NAME_1')
+AIRTABLE_TABLE_NAME_2 = os.environ.get('AIRTABLE_TABLE_NAME_2')
+AIRTABLE_TABLE_NAME_3 = os.environ.get('AIRTABLE_TABLE_NAME_3')
 
-endpoint = f'https://api.airtable.com/v0/{AIRTABLE_BASE_ID}/{AIRTABLE_TABLE_NAME}'
+
+# computer science, electrical eng, mechanical eng
+def get_table_name_by_field(field_name):
+    if field_name == "CS":
+        return AIRTABLE_TABLE_NAME_1
+    elif field_name == "EE":
+        return AIRTABLE_TABLE_NAME_2
+    elif field_name == "ME":
+        return AIRTABLE_TABLE_NAME_3
 
 
-def add_to_airtable(data_):
+def add_to_airtable(data_, field_name):
+    endpoint = f'https://api.airtable.com/v0/{AIRTABLE_BASE_ID}/{get_table_name_by_field(field_name)}'
     headers = {
         "Authorization": f'Bearer {AIRTABLE_API_KEY}',
         "Content-Type": "application/json"
@@ -24,48 +35,67 @@ def add_to_airtable(data_):
         print(r.json())
 
 
-
 # input list of jobs
-def skills_to_list(skills):
-    skills_list = skills.split("#")
+def skills_to_list(skills_):
+    skills_list = skills_.split("#")
 
     return skills_list
 
 
-def add_jobs_list_to_airtable(jobs_list):
-
+def add_jobs_list_to_airtable(jobs_list, field_name):
     data = []
     size = 0
     for job in jobs_list:
         record = {
-                  "fields": {"job_title": job[1],
-                             "company": job[2],
-                             "experience": job[12],
-                             "experience_years": job[9],
-                             "posting_time": job[5],
-                             "applicant_count": job[6],
-                             "location": job[4],
-                             "job_type": job[8],
-                             "degree_type": job[10],
-                             "skills": skills_to_list(job[11]),
-                             "link": job[0]
-                             }
-                  }
+            "fields": {"job_title": job[1],
+                       "company": job[2],
+                       "experience": job[12],
+                       "experience_years": job[9],
+                       "posting_time": job[5],
+                       "applicant_count": job[6],
+                       "location": job[4],
+                       "job_type": job[8],
+                       "degree_type": job[10],
+                       "skills": skills_to_list(job[11]),
+                       "link": job[0]
+                       }
+        }
 
         data.append(record)
         size += 1
         # max number to add in patch is 10
         if size == 10:
-            data_ = {"records": data,"typecast": True}
-            add_to_airtable(data_)
+            data_ = {"records": data, "typecast": True}
+            add_to_airtable(data_, field_name)
             data = []
             size = 0
 
     if len(data):
-        data_ = {"records": data,"typecast": True}
-        add_to_airtable(data_)
+        data_ = {"records": data, "typecast": True}
+        add_to_airtable(data_, field_name)
 
-skills = ['a','b']
+
+# field name options "CS","EE","ME"
+def delete_all_records(field_name):
+    base_url = f'https://api.airtable.com/v0/{AIRTABLE_BASE_ID}/{get_table_name_by_field(field_name)}'
+    headers = {
+        'Authorization': f'Bearer {AIRTABLE_API_KEY}',
+        'Content-Type': 'application/json'
+    }
+    response = requests.get(base_url, headers=headers)
+    data = response.json()
+    record_ids = [record['id'] for record in data['records']]
+    for record_id in record_ids:
+        delete_url = f'{base_url}/{record_id}'
+        requests.delete(delete_url, headers=headers)
+    print("All records have been deleted.")
+
+
+#### testing #######
+# delete_all_records()
+
+skills = ['a', 'b']
+
 # jobs_list_temp = [
 #     ['Software Engineer', 'ABC Company','Experience needed' , '3 years', ['New York'], 'Full-time',skills , 'https://example.com/job1'],
 #     ['Data Scientist', 'XYZ Corporation', 'Experience needed', '2 years', ['San Francisco'], 'Remote',skills , 'https://example.com/job2'],
@@ -82,22 +112,3 @@ skills = ['a','b']
 # # jobs_list_temp = [["a","dfsasda","None","sadasd",["New York"],"part time",["fffff"],"wwww.google.com"]]
 #
 # add_jobs_list_to_airtable(jobs_list_temp)
-
-
-
-def delete_all_records():
-    base_url = f'https://api.airtable.com/v0/{AIRTABLE_BASE_ID}/{AIRTABLE_TABLE_NAME}'
-    headers = {
-        'Authorization': f'Bearer {AIRTABLE_API_KEY}',
-        'Content-Type': 'application/json'
-    }
-    response = requests.get(base_url, headers=headers)
-    data = response.json()
-    record_ids = [record['id'] for record in data['records']]
-    for record_id in record_ids:
-        delete_url = f'{base_url}/{record_id}'
-        requests.delete(delete_url, headers=headers)
-    print("All records have been deleted.")
-
-
-# delete_all_records()
