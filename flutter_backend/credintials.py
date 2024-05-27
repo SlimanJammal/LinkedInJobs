503b17eac36447aada54269c862d0739a70c74fa
Sliman Jammal <100062609+SlimanJammal@users.noreply.github.com>
2024-05-27 17:23:55 +0300

    Delete flutter_backend/credintials.py

--- flutter_backend/credintials.py
+++
@@ -1,9 +0,0 @@
-linkedin_password = 'Fishpassword@123456789'
-linkedin_username = 'berem77489@mcatag.com'
-
-
-#Database
-db_host="localhost"
-db_user="root"
-db_password="Fishpassword"
-db_name="testdatabase"
