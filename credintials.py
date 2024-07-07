66cae05606f089c1b54e71dc53b4d828a533696b
Mohammad-Eid <m7mad20015@gmail.com>
2024-07-07 20:46:34 +0300

    added the instruction to the second md file

---
+++ credintials.py
@@ -0,0 +1,9 @@
+linkedin_password = 'Replace_With_Your_LinkedIn_Password'
+linkedin_username = 'Replace_With_Your_LinkedIn_Username'
+
+
+#Database
+db_host="Replace_With_Your_DBHost"
+db_user="Replace_With_Your_DBUser"
+db_password="Replace_With_Your_DB_Password"
+db_name="Replace_With_Your_DBName"
